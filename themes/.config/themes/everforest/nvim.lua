e0227bede226d7a1db857f6c6743cde3d34f66a1
Joseph Herrera <josephherreraleon@gmail.com>
2025-11-10 19:32:35 -0600

    feature(theme): add catppuccin,gruvbox, onedarkpro and tokyo night themes

--- themes/.config/themes/everforest/nvim.lua
+++ themes/.config/themes/everforest/nvim.lua
@@ -1,7 +1,12 @@
-return 
-  { "neanias/everforest-nvim", lazy = false, priority = 1000,
-    config = function()
-      require("everforest").setup({})
-      vim.cmd.colorscheme("everforest")
-    end
-  }
+return {
+  "neanias/everforest-nvim",
+  version = false,
+  lazy = false,
+  priority = 1000,
+  config = function()
+    require("everforest").setup({
+      transparent_background_level = 1,
+    })
+    vim.cmd([[colorscheme everforest]])
+  end,
+}
